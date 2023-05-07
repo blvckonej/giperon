7ded2f687301dac5e569a7c9fc7b457b221dfc9b
Maxim Razin <blvckonej@gmail.com>
2023-05-07 18:44:11 +0300

    flip services card

--- components/services/services.js
+++ components/services/services.js
@@ -2,14 +2,14 @@ class Products {
     render() {
         let htmlCatalog = '';
 
-        CATALOG.forEach(({id, name, img, descr, className}) => {
+        CATALOG.forEach(({id, name, img, descr, className, backClassName}) => {
             htmlCatalog += `
             <div class="services__block-flip" >
                 <div class="services__block-product front ${className}">
                     <p class="services__block-subtitle">${name}</p>
                     <a href="#" class="services__block-link">Узнать подробности <img src="./img/rarrow.svg" alt="Arrow icon"></a>
                 </div>
-                <div class="services__block-backproduct back">
+                <div class="services__block-backproduct ${backClassName} back">
                     <p class="services__block-subtitle">${name}</p>
                     <p class="services__block-backdescr">${descr}</p>
                     <a href="#" class="services__block-link">Узнать подробности <img src="./img/blackarrow.svg" alt="Arrow icon"></a>
