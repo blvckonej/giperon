9e35042e7032834d94a4f4e68bc125d9f34d0d0b
Maxim Razin <blvckonej@gmail.com>
2023-05-07 16:00:50 +0300

    added js card

---
+++ components/services/services.js
@@ -0,0 +1,31 @@
+class Products {
+    render() {
+        let htmlCatalog = '';
+
+        CATALOG.forEach(({id, name, img, descr, className}) => {
+            htmlCatalog += `
+            <div class="services__block-flip" >
+                <div class="services__block-product front ${className}">
+                    <p class="services__block-subtitle">${name}</p>
+                    <a href="#" class="services__block-link">Узнать подробности <img src="./img/rarrow.svg" alt="Arrow icon"></a>
+                </div>
+                <div class="services__block-backproduct back">
+                    <p class="services__block-subtitle">${name}</p>
+                    <p class="services__block-backdescr">${descr}</p>
+                    <a href="#" class="services__block-link">Узнать подробности <img src="./img/blackarrow.svg" alt="Arrow icon"></a>
+                </div>
+            </div>
+            `
+        })
+        const htmlRender = `
+
+        ${htmlCatalog}
+         
+        `;
+
+        ROOT_PRODUCTS.innerHTML = htmlRender;
+    }
+}
+
+const productsPage = new Products();
+productsPage.render();
